768adf72d5b871e38ad0df61b13256e00b31f361
johnweikangong <ongwk95@hotmail.com>
2019-03-15 16:53:54 +0800

    first commit

---
+++ EE2028Asmt2S2AY201819/Debug/src/main.d
@@ -0,0 +1,22 @@
+src/main.d src/main.o: ../src/main.c \
+  F:\EE2024workspace\Lib_MCU\inc/lpc17xx_pinsel.h \
+  F:\EE2024workspace\Lib_CMSISv1p30_LPC17xx\inc/LPC17xx.h \
+  F:\EE2024workspace\Lib_CMSISv1p30_LPC17xx\inc/core_cm3.h \
+  F:\EE2024workspace\Lib_CMSISv1p30_LPC17xx\inc/system_LPC17xx.h \
+  F:\EE2024workspace\Lib_MCU\inc/lpc_types.h \
+  F:\EE2024workspace\Lib_MCU\inc/lpc17xx_gpio.h \
+  F:\EE2024workspace\Lib_MCU\inc/lpc17xx_timer.h
+
+F:\EE2024workspace\Lib_MCU\inc/lpc17xx_pinsel.h:
+
+F:\EE2024workspace\Lib_CMSISv1p30_LPC17xx\inc/LPC17xx.h:
+
+F:\EE2024workspace\Lib_CMSISv1p30_LPC17xx\inc/core_cm3.h:
+
+F:\EE2024workspace\Lib_CMSISv1p30_LPC17xx\inc/system_LPC17xx.h:
+
+F:\EE2024workspace\Lib_MCU\inc/lpc_types.h:
+
+F:\EE2024workspace\Lib_MCU\inc/lpc17xx_gpio.h:
+
+F:\EE2024workspace\Lib_MCU\inc/lpc17xx_timer.h:
